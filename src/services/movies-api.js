87d43bb5b6a0ffed5101c0fd652bbdd17f408afd
Vitaliy7171 <vitaliynekrasov1@gmail.com>
2022-11-22 16:41:55 +0200

    Update movies-api.js

--- src/services/movies-api.js
+++ src/services/movies-api.js
@@ -1,34 +1,47 @@
 import axios from 'axios';
 
 const API_KEY = '1c308592d719c7668d47adc63622f209';
+const baseURL = 'https://api.themoviedb.org/3/';
 
 export async function getTrendingMovies() {
   const resp = await axios.get(
-    `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
+    `${baseURL}trending/movie/week?api_key=${API_KEY}`
   );
   const result = await resp.data.results;
   return result;
 }
 
 export async function getMovies(query) {
-  const resp = await axios.get(
-    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`
-  );
+  // const searchParams = {
+  //   api_key: API_KEY,
+  //   query: query,
+  // };
+  const obj = {
+    method: 'get',
+    url: `${baseURL}search/movie`,
+    params: {
+      api_key: API_KEY,
+      query: query,
+    },
+  };
+
+  // const resp = await axios.get(
+  //   `${baseURL}search/movie?api_key=${API_KEY}&query=${query}`
+  // );
+  const resp = await axios(obj);
   const result = await resp.data.results;
   return result;
 }
 
 export async function getMoviesById(id) {
-  const resp = await axios.get(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`
-  );
+  const resp = await axios.get(`${baseURL}movie/${id}?api_key=${API_KEY}`);
   const result = await resp.data;
   return result;
 }
 
 export async function getMoviesCast(id) {
   const resp = await axios.get(
-    `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${API_KEY}&language=en-US`
+    `${baseURL}movie/${id}/credits?api_key=${API_KEY}`
   );
   const result = await resp.data.cast;
   return result;
@@ -36,7 +49,7 @@ export async function getMoviesCast(id) {
 
 export async function getMoviesReviews(id) {
   const resp = await axios.get(
-    `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${API_KEY}&language=en-US`
+    `${baseURL}movie/${id}/reviews?api_key=${API_KEY}`
   );
   const result = await resp.data.results;
   return result;
